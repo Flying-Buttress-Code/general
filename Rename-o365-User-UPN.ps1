afabe812ea11d422e7adb2b81c3c1738b2dd6419
Graham-FB <87187963+Graham-FB@users.noreply.github.com>
2021-07-09 13:26:50 -0500

    Add files via upload

---
+++ Rename-o365-User-UPN.ps1
@@ -0,0 +1,10 @@
+﻿#Rename o365 account UPN
+
+#Connect to MSOL Service
+Connect-MsolService
+
+#Verify current user's properties
+Get-MsolUser -UserPrincipalName current-username@domain.onmicrosoft.com |select DisplayName,SignInName,UserPrincipalName
+
+#Rename user account
+Set-msoluserprincipalname -userprincipalname current-username@domain.onmicrosoft.com.com -newuserprincipalname new-username@whateverdomain.com
