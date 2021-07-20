7d97e0f31616f017ce6a0bc31411e3fe8e9ccdc7
Graham-FB <87187963+Graham-FB@users.noreply.github.com>
2021-07-20 09:02:59 -0500

    Add files via upload

---
+++ Disable-SelfPurchase-TenantWide.ps1
@@ -0,0 +1,30 @@
+﻿#Disable Self-Service for all products in the Tenant. 
+
+#Install MSCommerce Module
+
+Install-Module -Name MSCommerce
+
+#Import MSCommerce Module
+
+Import-Module -Name MSCommerce
+
+#Get all self service purchases and disable self service ability
+
+$allproducts=Get-MSCommerceProductPolicies -PolicyId AllowSelfServicePurchase
+
+Foreach($ProductName in $allproducts){
+Update-MSCommerceProductPolicy -PolicyId AllowSelfServicePurchase -ProductID $ProductName.ProductID -Enabled $False
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
