2e9e402c2b73620b42f238c875216b43cce540e2
Graham-FB <87187963+Graham-FB@users.noreply.github.com>
2021-08-20 11:02:48 -0500

    Add files via upload
    This is a good set of commands for instantly revoking access to o365/Azure services. The first flag disables the user using the same method as disabling via the GUI. The second revokes all active sessions which can sometime linger for 60+ mins after disabling the account.

---
+++ Disable-Revoke-User-by-UPN.ps1
@@ -0,0 +1,13 @@
+﻿#Connect to Azure AD
+Connect-AzureAD
+
+#Get current user status
+Get-AzureADUser -ObjectId userUPNhere@flyingbuttress.com | fl
+
+#Disable account
+Set-AzureADUser -ObjectId userUPNhere@flyingbuttress.com -AccountEnabled $false
+
+#Revoke all current access tokens
+Revoke-AzureADUserAllRefreshToken -ObjectId userUPNhere@flyingbuttress.com
+
+
