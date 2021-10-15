e4207bd8621252ba79facdd8ce1e85062adbf64e
Graham-FB <87187963+Graham-FB@users.noreply.github.com>
2021-10-15 11:20:15 -0500

    Collects all shared mailboxes & blocks sigin o365

---
+++ BlockSignIn-AllSharedMailboxes.ps1
@@ -0,0 +1,42 @@
+﻿
+#Connect to both services / verify environment 
+
+Connect-ExchangeOnline
+Connect-MsolService
+
+
+
+#Start Collect Shared Mailboxes.
+ 
+$Users = Get-Mailbox -ResultSize unlimited -Filter "(RecipientTypeDetails -eq 'SharedMailbox')" | select PrimarySmtpAddress
+
+#List Shared Mailboxes
+ 
+foreach ($user in $Users){
+
+Get-MsolUser -UserPrincipalName $user.PrimarySmtpAddress | select UserPrincipalName,BlockCredential
+
+}
+
+read-host -Prompt “**Press ENTER to continue to block sign-in for these accounts...**” 
+
+#Set all to blocked
+
+foreach ($user in $Users){
+
+Set-MsolUser -UserPrincipalName $user.PrimarySmtpAddress -BlockCredential $true
+
+}
+
+
+#End Setting
+
+write-host "**New Block Credential Settings**" -ForegroundColor Green
+
+foreach ($user in $Users){
+
+Get-MsolUser -UserPrincipalName $user.PrimarySmtpAddress | select UserPrincipalName,BlockCredential
+
+}
+
+
